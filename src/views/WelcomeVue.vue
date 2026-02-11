ef378bc486d97c0173a2107f9739137083a6ebd4
BAOfanTing <1316149505@qq.com>
2026-02-11 21:13:44 +0800

    init

---
+++ src/views/WelcomeVue.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class = "welcom">
+        hello!
+    </div>
+    <el-button type="primary">Primary</el-button>
+    <el-icon :size="size" :color="color">
+      <Edit />
+    </el-icon>
+</template>
